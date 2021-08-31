3854ec889f8b08a0ec0fac3f478a29af7cb74b2e
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2021-08-31 09:21:35 -0400

    Support checking file extensions regardless of case

--- src/main/java/me/tagavari/airmessageserver/helper/FileHelper.java
+++ src/main/java/me/tagavari/airmessageserver/helper/FileHelper.java
@@ -9,6 +9,6 @@ public class FileHelper {
     public static Optional<String> getExtensionByStringHandling(String filename) {
         return Optional.ofNullable(filename)
                 .filter(f -> f.contains("."))
-                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
+                .map(f -> f.substring(filename.lastIndexOf(".") + 1).toLowerCase());
     }
 }
